7c9d4383182f7c1e21bee3e0a4165d9a392d9604
Schleder <luisschleder1@gmail.com>
2023-02-06 23:30:45 -0300

    Tada!

--- README.md
+++
@@ -1,1 +0,0 @@
-# projetoDevLinks
---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+ const html = document.documentElement;
+ html.classList.toggle('light')
+
+ // pegar a tag img
+ const img = document.querySelector("#profile img ")
+
+ // substituir a imagem
+ if(html.classList.contains('light')) {
+  // se estiver em light mode, mudar a img
+  img.setAttribute("src", "./assets/mobile/-light.png")
+ }else{
+  // se nao, manter a mesma img
+  img.setAttribute("src", "./assets/mobile/Avatar.png")
+ } 
+
+}
